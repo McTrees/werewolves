78ec7a1c963998b72c7be805769a0352b2ca4581
ed588 <calvin.588.edwards@gmail.com>
2018-01-05 14:16:33 +0000

    change base64 fns to not use depreciated buffer methods

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 exports.toBase64 = function (str) {
-  return new Buffer(str).toString("base64");
+  return Buffer.from(str).toString("base64");
 };
 
 exports.fromBase64 = function (b64) {
-  return new Buffer(b64, "base64").toString("utf8")
+  return Buffer.from(b64, "base64").toString("utf8")
 };
