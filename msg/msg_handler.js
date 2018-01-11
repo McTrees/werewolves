866fbd2efc6ee941c0f0f86c1940c5b220163b7c
ed588 <calvin.588.edwards@gmail.com>
2018-01-11 18:40:14 +0000

    add test command for confirm

--- msg/msg_handler.js
+++ msg/msg_handler.js
@@ -10,5 +10,7 @@ module.exports = function(msg, client) {
   // TODO: make this a proper lookup thing and not just a big if/else
   if (msg.content.startsWith("!signup")) {
     require("../user/user").signupCmd(msg, client);
+  } else if (msg.content.startsWith("!confirm")) {
+    require("../admin/admin").confirm("test test test", client)
   }
 };
