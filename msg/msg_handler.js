a74c8a1a6bfc20f5493358bcdea68acbab865749
trebor97351 <adds.robbie@gmail.com>
2018-01-13 16:51:35 +0000

    fixed gitignore

--- msg/msg_handler.js
+++ msg/msg_handler.js
@@ -1,19 +1,24 @@
-const user = require("../user/user")
-const admin = require("../admin/admin")
-const config = require('../config');
 /* this file recieves the message object
    and decides what do do with it
 
    it calls functions from other files with the message object
 */
 
+
+const config = require('../config');
 module.exports = function(msg, client) {
   if (msg.author == client.user) return; //ignore own messages
-  // TODO: make this a proper lookup thing and not just a big if/else
-  // that would involve a separate lookup table
-  if (msg.content.startsWith("!signup")) {
-    require("../user/user").signupCmd(msg, client);
-  } else if (msg.content.startsWith("!confirm")) {
-    require("../admin/admin").confirm("test test test", client)
-  }
+  messageContent = msg.content.split(" ");
+  if (messageContent[0][0] == config.bot_prefix){
+    messageContent[0] = messageContent[0].slice(1);
+    switch(messageContent[0]){
+      case ("u"):
+        try{
+        console.log(messageContent[1]+"Cmd");
+        }catch(err){
+          console.log("invalid command");
+        }
+        break;
+      }
+  };
 };
