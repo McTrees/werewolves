521ceb955dc1cddd9858f63979842ff319ef3d5b
oliverh57 <oliver.hynds@gmail.com>
2018-01-16 21:35:33 +0000

    more fixes

--- cc/ccs.js
+++ cc/ccs.js
@@ -2,12 +2,12 @@ const config = require('../config'); //include main config
 const configcc = require('./configcc'); //include config for CCs
 
 exports.createCmd = function(msg, clinet, args){//mgs = message obdj, client = bot client obdj, args = array of arguments
-  msg.delete(1000);// del message
+  msg.delete();// del message
   makeChannel(msg, args[0]);
 }
 
 function makeChannel(message, name){//function to create
     var server = message.guild; //set server to the server message was sent from
-
+    name = "CC_S" + config.season + "_" + name
     server.createChannel(name, "text");
 }
