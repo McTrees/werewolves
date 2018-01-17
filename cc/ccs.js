9a6b8a10987e690783013e8a62f2d1af735be9ad
oliverh57 <oliver.hynds@gmail.com>
2018-01-17 17:45:52 +0000

    more files

--- cc/ccs.js
+++ cc/ccs.js
@@ -1,16 +1,28 @@
 const config = require('../config'); //include main config
 const configcc = require('./configcc'); //include config for CCs
+const fs = require('fs'); //require lib for files
+
+let student = {
+    name: 'Mike',
+    age: 23,
+    gender: 'Male',
+    department: 'English',
+    car: 'Honda'
+};
+
+var data = JSON.stringify(student);
 
 exports.createCmd = function(msg, clinet, args){//mgs = message obdj, client = bot client obdj, args = array of arguments
   msg.delete();// del message
   makeChannel(msg, args[0]);
 }
 
-function makeChannel(message, name){//function to create
+  function makeChannel(message, name){//function to create
   var currentcatgory = "402933080119312398"; //ID of the currentcatgory that channels are stored in. NEEDS WORK
   var server = message.guild; //set server to the server message was sent from
 
   if(name == "" || name.split(0) == "@"){ //test to see if there are no arguments or if name should be thingy
+    fs.writeFileSync('./configcc', data);
     name = "S" + config.season + "_CC_" + name; //phrase name of catgory
 
     server.createChannel(name, "text").then(channel => //creates channels
