7c6d52d1818c3531d8fd8a1a63685ce79fba5509
oliverh57 <oliver.hynds@gmail.com>
2018-01-17 17:11:23 +0000

    checks

--- cc/ccs.js
+++ cc/ccs.js
@@ -7,12 +7,18 @@ exports.createCmd = function(msg, clinet, args){//mgs = message obdj, client = b
 }
 
 function makeChannel(message, name){//function to create
-    var server = message.guild; //set server to the server message was sent from
-    //var server = message.catgory; //set server to the server message was sent from
+  var currentcatgory = "402933080119312398"; //ID of the currentcatgory that channels are stored in. NEEDS WORK
+  var server = message.guild; //set server to the server message was sent from
+
+  if(name == "" || name.split(0) == "@"){ //test to see if there are no arguments or if name should be thingy
     name = "S" + config.season + "_CC_" + name; //phrase name of catgory
-    var currentcatgory = "402933080119312398"; //ID of the currentcatgory that channels are stored in. NEEDS WORK
 
     server.createChannel(name, "text").then(channel => //creates channels
       channel.setParent(server.channels.get(currentcatgory))).then(channel => //move new channel into currentcatgory
         channel.send(config.defaultMessage)); //sends defaut message
+
+  }else{
+    message.reply("lol syntax brov")
+  }
+
 }
