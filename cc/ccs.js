96586b01111fc13467eeaa3dc6e355539ec89616
oliverh57 <oliver.hynds@gmail.com>
2018-01-17 16:58:02 +0000

    added coments

--- cc/ccs.js
+++ cc/ccs.js
@@ -9,11 +9,10 @@ exports.createCmd = function(msg, clinet, args){//mgs = message obdj, client = b
 function makeChannel(message, name){//function to create
     var server = message.guild; //set server to the server message was sent from
     //var server = message.catgory; //set server to the server message was sent from
-    name = "S" + config.season + "_CC_" + name;
-    var defaultMessage = "welcome to the new channel";
-    var currentcatgory = "402933080119312398";
+    name = "S" + config.season + "_CC_" + name; //phrase name of catgory
+    var currentcatgory = "402933080119312398"; //ID of the currentcatgory that channels are stored in. NEEDS WORK
 
-    server.createChannel(name, "text").then(channel =>
-      channel.setParent(server.channels.get(currentcatgory))).then(channel =>
-        channel.send(config.defaultMessage));
+    server.createChannel(name, "text").then(channel => //creates channels
+      channel.setParent(server.channels.get(currentcatgory))).then(channel => //move new channel into currentcatgory
+        channel.send(config.defaultMessage)); //sends defaut message
 }
