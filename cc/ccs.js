73102b0146675d212732f3190c74a10a88c74e9e
oliverh57 <oliver.hynds@gmail.com>
2018-01-17 17:55:23 +0000

    config

--- cc/ccs.js
+++ cc/ccs.js
@@ -2,15 +2,6 @@ const config = require('../config'); //include main config
 const configcc = require('./configcc'); //include config for CCs
 const fs = require('fs'); //require lib for files
 
-let student = {
-    name: 'Mike',
-    age: 23,
-    gender: 'Male',
-    department: 'English',
-    car: 'Honda'
-};
-
-var data = JSON.stringify(student);
 
 exports.createCmd = function(msg, clinet, args){//mgs = message obdj, client = bot client obdj, args = array of arguments
   msg.delete();// del message
@@ -22,15 +13,14 @@ exports.createCmd = function(msg, clinet, args){//mgs = message obdj, client = b
   var server = message.guild; //set server to the server message was sent from
 
   if(name == "" || name.split(0) == "@"){ //test to see if there are no arguments or if name should be thingy
-    fs.writeFileSync('./configcc', data);
+    message.reply("Incorenct syntax; you must specify a name *note: it cant start with `@`*")
+  }else{
     name = "S" + config.season + "_CC_" + name; //phrase name of catgory
 
     server.createChannel(name, "text").then(channel => //creates channels
       channel.setParent(server.channels.get(configcc.CC_curent_id))).then(channel => //move new channel into currentcatgory
         channel.send(config.defaultMessage)); //sends defaut message
 
-  }else{
-    message.reply("Incorenct syntax; you must specify a name *note: it cant start with `@`*")
   }
 
 }
