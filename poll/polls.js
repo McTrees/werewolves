03e780d57604627a7dc689702c765c0b43825df7
Lord-of-the-Galaxy <prathikdiwakar@gmail.com>
2018-01-18 17:21:34 +0530

    Added comments

--- poll/polls.js
+++ poll/polls.js
@@ -25,27 +25,56 @@ exports.startPollCmd = function(msg, client, args){
 			
 			break;
 		default:
-			msg.reply("I'm sorry, but `" + type + "` is not a valid poll");
+			msg.reply("I'm sorry, but `" + type + "` is not a valid poll type");
 	}
 	//Send message informing GMs of new poll
 	if(id !== -1)client.channels.get(config.channel_ids.gm_confirm).send("A new Poll, ``"+txt+"`` (id: "+num+") was created.");
 }
 
-exports.startPollActual = function(msg, client, data){
-	//Test code
-	/**
-	var json = (`[{"txt":"Option 1","emoji":"💩"},{"txt":"Option 2","emoji":"😃"}]`);
-	var options = JSON.parse(json);
-	var msg_text = "Vote for your favorite!";
-	*/
-	//Actual code
+
+/**
+	Function startPollActual
+	Start a poll.
+	Arguments:
+	client - The Discord Client the bot uses
+	data - An object containing three properties - msg_text, channel_id, options:
+		msg_text - A String, the text that is displayed at the start of the poll.
+		channel_id - The ID of the channel in which to have the poll
+		options - An array, the list of choices in the poll, along with thier emojis. Each element has two properties - txt, emoji:
+			txt - The text corresponding to the option
+			emoji - The emoji corresponding to the option (It's just a character BTW, just like all emojis in discord)
+			
+	Example for data:
+	{
+		msg_text:"Vote for your favourite!",
+		channel_id:"4034578342784532XX",
+		options:[
+			{
+				txt:"<@329977469350445069>",
+				emoji:"😃"	
+			},
+			{
+				txt:"<@402072907284480000>",
+				emoji:"😕"
+			},
+			{
+				txt:"A",
+				emoji:"💀"
+			}
+		]
+	}
+	As in example, the "txt" field of the options can be a mention or just some plain text
+*/
+//I'm exporting this just in case it's needed sometime
+exports.startPollActual = function(client, data){
 	
 	var options = data.options;
 	var msg_text = data.msg_text;
 	
 	var ch = client.channels.get(data.channel_id);
-	var nm = (options.length - ((options.length-1)%20 + 1))/20 + 1;
-	var txt = new Array(nm);
+	
+	var nm = (options.length - ((options.length-1)%20 + 1))/20 + 1;//Number of messages the bot must send
+	var txt = new Array(nm);//The text of the messages themselves
 	for(i = 0; i < nm; i++){
 		txt[i] = "";
 		if(i === 0)txt[0] = msg_text+"-\n";
@@ -55,11 +84,15 @@ exports.startPollActual = function(msg, client, data){
 			txt[i] += "\n";
 		}
 	}
+	
+	//This will send the messages, and get the Promises of the messages
 	var promises = new Array(nm);
 	for(i = 0; i < nm; i++){
-		promises[i] = msg.channel.send(txt[i]);
+		promises[i] = ch.send(txt[i]);
 	}
+	//Combine all the promises
 	Promise.all(promises).then(values => {
+		//This whole code just adds the emojis
 		msgs = new Array(values.length);
 		for(i = 0; i < values.length; i++){
 			msgs[i] = {
@@ -78,29 +111,40 @@ exports.startPollActual = function(msg, client, data){
 			}
 			msgs[i]["options"] = opts;
 		}
+		//Now save the poll so that a restart of the bot doesn't delete all the data
 		polls["num"]++;
 		var num = polls["num"];
 		//Here I'm saving some stuff twice. It makes my work easier, but it's not storage efficient.
 		//Though again, an extra kilobyte or two isn't much
 		polls["polls"][num] = {
-			channel:msg.channel.id,
+			channel:ch.id,
 			messages:msgs,
 			options:options
 		};
 		fs.writeFile("./poll/polls.json", JSON.stringify(polls, null, 2), (err) => {
 			if (err) console.error(err)
 		});
-	}).catch(console.error);
-	return polls["num"]+1;
+	}).catch(console.error);//I'm not sure if this is correct way, but I've seen people do it
+	return polls["num"]+1;//Return the ID of the poll
 }
 
+/**
+	Function - checkPollCmd
+	Checks if all the emojis have been added to the poll
+	Arguments:
+	msg - The message that triggered the function
+	client - The Discord Client that the bot uses
+	id - The ID of the poll to check
+*/
 exports.checkPollCmd = function(msg, client, id){
 	if(!polls["polls"][id]){
 		console.log("The poll with id " + id + " doesn't exist, sadly. I haven't thought of what to do in that case.");
 		return;
 	}
+	//Get the poll and its details
 	var poll = polls["polls"][id];
 	var ch = client.channels.get(poll["channel"]);
+	//Get the messages (promises of the messages really)
 	var promises = new Array(poll["messages"].length);
 	for(i = 0; i < promises.length; i++){
 		promises[i] = msg.channel.fetchMessage(poll["messages"][i].id);
@@ -108,6 +152,7 @@ exports.checkPollCmd = function(msg, client, id){
 	Promise.all(promises).then(msgs => {
 		for(i = 0; i < poll["messages"].length; i++){
 			for(j = 0; j < poll["messages"][i]["options"].length; j++){
+				//Check if the message has all required emojis, add the missing ones.
 				var r = msgs[i].reactions.find(val => val.emoji.name === poll["messages"][i]["options"][j]["emoji"]);
 				if(!r || !r.me)msgs[i].react(poll["messages"][i]["options"][j]["emoji"]).catch("Not again! :(");
 			}
@@ -116,18 +161,30 @@ exports.checkPollCmd = function(msg, client, id){
 	}).catch(console.error);
 }
 
-exports.endPollCmd = function(msg, client, id){//I need the client because that's how I'm gonna find the channel. I should make this better sometime.
+/**
+	Function - endPollCmd
+	Ends a poll
+	Arguments:
+	msg - The message that triggered the function
+	client - The Discord Client that the bot uses
+	id - The ID of the poll to end
+*/
+exports.endPollCmd = function(msg, client, id){
 	if(!polls["polls"][id]){
 		console.log("The poll with id " + id + " doesn't exist, sadly. I haven't thought of what to do in that case.");
 		return;
 	}
+	//Fetch the poll and its details
 	var poll = polls["polls"][id]; 
 	var ch = client.channels.get(poll["channel"]);
+	//Get the messages
 	var promises = new Array(poll["messages"].length);
 	for(i = 0; i < promises.length; i++){
 		promises[i] = ch.fetchMessage(poll["messages"][i].id);
 	}
+	//Work on the messages, and then return a promise of the results
 	return Promise.all(promises).then(msgs => {
+		//Get the message reactions
 		var promises = new Array(poll["options"].length);
 		var s = 0;
 		for(i = 0; i < poll["messages"].length; i++){
@@ -139,14 +196,18 @@ exports.endPollCmd = function(msg, client, id){//I need the client because that'
 		}
 		//WARNING - This code is not as efficient as it should be
 		return Promise.all(promises).then(function(values){
+			//The text message the bot will send
 			var txt = "Results of the polls:\n";
+			//The object the function will return
 			var results = {
 				id:id,
 				options:poll["options"]
 			};
+			//Disqualified persons
 			var disqualified = new Array(0);
-			var voted = new Array(0);
+			var voted = new Array(0);//Who as voted?
 			for(i = 0; i < values.length; i++){
+				//Check if person has voted twice -> disqualify them
 				var users = Array.from(values[i]);
 				users.forEach(function(item){
 					if(item[1].id !== client.user.id){
@@ -160,10 +221,11 @@ exports.endPollCmd = function(msg, client, id){//I need the client because that'
 							voted.push(item[1].id);
 						}
 					}else{
-						values[i].delete(item[0]);
+						values[i].delete(item[0]);//Forget the reactions the bot itself sent
 					}
 				});
-			}						
+			}
+			//Delete the votes of the disqualified
 			for(i = 0; i < values.length; i++){
 				var users = Array.from(values[i]);	
 				users.forEach(function(item){
@@ -172,9 +234,11 @@ exports.endPollCmd = function(msg, client, id){//I need the client because that'
 					}))values[i].delete(item[0]);
 				});
 			}
+			
+			//Rank the results of the poll (descending order)
 			var ranked = new Array(0);
 			for(i = 0; i < values.length; i++){	
-				results.options[i].votes = values[i].size;
+				results.options[i].votes = values[i].size;//Also add the vote tally to the results object
 				if(values[i].size === 0)continue;
 				ranked.push({
 					id:i,
@@ -184,6 +248,7 @@ exports.endPollCmd = function(msg, client, id){//I need the client because that'
 			ranked.sort(function(a, b){
 				return b.num - a.num;
 			});
+			//Build the message to be sent
 			for(k = 0; k < ranked.length; k++){
 				var i = ranked[k].id;
 				var users = Array.from(values[i]);
@@ -192,6 +257,7 @@ exports.endPollCmd = function(msg, client, id){//I need the client because that'
 					txt += ("\t<@" + users[j][1].id + ">\n");
 				}
 			}
+			//And make sure to mention who was disqualified
 			if(disqualified.length !== 0){
 				txt += "\n";
 				if(disqualified.length === 1){
@@ -205,114 +271,14 @@ exports.endPollCmd = function(msg, client, id){//I need the client because that'
 					txt += " were disqualified as they cast multiple votes.";
 				}
 			}
+			//Send the message - Hooray!
 			ch.send(txt);
-			
+			//Delete the poll from storage
 			delete polls["polls"][id];
 			fs.writeFile("./poll/polls.json", JSON.stringify(polls, null, 2), (err) => {
 				if (err) console.error(err)
 			});
-			
-			for(i = 0; i < msgs.length; i++){
-				msgs[i].delete();
-			}
-			//Return the data
-			return results;
-		});
-	});if(!polls["polls"][id]){
-		console.log("The poll with id " + id + " doesn't exist, sadly. I haven't thought of what to do in that case.");
-		return;
-	}
-	var poll = polls["polls"][id]; 
-	var ch = client.channels.get(poll["channel"]);
-	var promises = new Array(poll["messages"].length);
-	for(i = 0; i < promises.length; i++){
-		promises[i] = ch.fetchMessage(poll["messages"][i].id);
-	}
-	return Promise.all(promises).then(msgs => {
-		var promises = new Array(poll["options"].length);
-		var s = 0;
-		for(i = 0; i < poll["messages"].length; i++){
-			for(j = 0; j < poll["messages"][i]["options"].length; j++){
-				var r = msgs[i].reactions.find(val => val.emoji.name === poll["messages"][i]["options"][j]["emoji"]);
-				promises[s] = r.fetchUsers();
-				s++;
-			}
-		}
-		//WARNING - This code is not as efficient as it should be
-		return Promise.all(promises).then(function(values){
-			var txt = "Results of the polls:\n";
-			var results = {
-				id:id,
-				options:poll["options"]
-			};
-			var disqualified = new Array(0);
-			var voted = new Array(0);
-			for(i = 0; i < values.length; i++){
-				var users = Array.from(values[i]);
-				users.forEach(function(item){
-					if(item[1].id !== client.user.id){
-						if(voted.find(element => {
-							return element == item[1].id;
-						})){
-							if(!disqualified.find(element => {
-								return element == item[1].id;
-							}))disqualified.push(item[1].id);
-						}else{
-							voted.push(item[1].id);
-						}
-					}else{
-						values[i].delete(item[0]);
-					}
-				});
-			}						
-			for(i = 0; i < values.length; i++){
-				var users = Array.from(values[i]);	
-				users.forEach(function(item){
-					if(disqualified.find(element => {
-						return element == item[1].id;
-					}))values[i].delete(item[0]);
-				});
-			}
-			var ranked = new Array(0);
-			for(i = 0; i < values.length; i++){	
-				results.options[i].votes = values[i].size;
-				if(values[i].size === 0)continue;
-				ranked.push({
-					id:i,
-					num:values[i].size
-				});
-			}
-			ranked.sort(function(a, b){
-				return b.num - a.num;
-			});
-			for(k = 0; k < ranked.length; k++){
-				var i = ranked[k].id;
-				var users = Array.from(values[i]);
-				txt += "\n" + (users.length + " voted for " + poll["options"][i]["txt"] + " (" + poll["options"][i]["emoji"] + "):\n");
-				for(j = 0; j < users.length; j++){
-					txt += ("\t<@" + users[j][1].id + ">\n");
-				}
-			}
-			if(disqualified.length !== 0){
-				txt += "\n";
-				if(disqualified.length === 1){
-					txt += "<@" + disqualified[0] + "> was disqualified as they cast multiple votes.";
-				}else{
-					disqualified.forEach(function(item, index){
-						txt += "<@" + item + ">";
-						if(index === disqualified.length - 2)txt += " and ";
-						else if(index !== disqualified.length -1)txt += ", "
-					});
-					txt += " were disqualified as they cast multiple votes.";
-				}
-			}
-			ch.send(txt);
-			
-			delete polls["polls"][id];
-			fs.writeFile("./poll/polls.json", JSON.stringify(polls, null, 2), (err) => {
-				if (err) console.error(err)
-			});
-			
+			//Delete the messages
 			for(i = 0; i < msgs.length; i++){
 				msgs[i].delete();
 			}
