18a81a01f69d7bd905616df286fa2a9d8c3f3c35
trebor97351 <adds.robbie@gmail.com>
2018-01-20 13:45:36 +0000

    made CCs make new categories and updated cc.json to have category number

--- cc/ccs.js
+++ cc/ccs.js
@@ -1,37 +1,60 @@
  const config = require('../config'); //include main config
-var fs = require('fs')
+ var fs = require('fs')
 
-var ccconf; //decalre cc conf var as global
-exports.createCmd = function(msg, clinet, args){//mgs = message obdj, client = bot client obdj, args = array of arguments
-  msg.delete();// del message
-  makeChannel(msg, args[0]);
-}
+ var ccconf; //decalre cc conf var as global
+ exports.createCmd = function(msg, clinet, args) { //mgs = msg obdj, client = bot client obdj, args = array of arguments
+   msg.delete(); // del msg
+   makeChannel(msg, args[0]);
+ }
 
-function makeChannel(message, name){//functiohn to create
-  if(name == "" || name.split(0) == "@"){ //test to see if there are no arguments or if name should be thingy
-    message.reply("Incorenct syntax; you must specify a name *note: it cant start with `@`*")
-  }else{
-    fs.readFile('./cc/cc.json', {encoding: 'utf-8'}, function (err, data) { //read ccconfg.json to ccconfg
-      if (err) throw err; //throw error
-      ccconf = JSON.parse(data); //turns string into JSON object
+ function makeChannel(msg, name) { //functiohn to create
+   if (name == "" || name.split(0) == "@") { //test to see if there are no arguments or if name should be thingy
+     msg.reply("Incorenct syntax; you must specify a name *note: it cant start with `@`*")
+   } else {
+     fs.readFile('./cc/cc.json', {
+       encoding: 'utf-8'
+     }, function(err, data) { //read cc.json to ccconfig
+       if (err) throw err; //throw error
+       ccconf = JSON.parse(data); //turns string into JSON object
 
-      ccconf.CC_curent_number = ccconf.CC_curent_number + 1; //adds one to the number of CCs
-      message.reply("Current number of CCs: `" + ccconf.CC_curent_number + "` Current ID of cc catgory: `" + ccconf.CC_curent_id + "`")
+       name = "S" + config.season + "_CC_" + name; //phrase name of channel
+       ccconf.CC_catagory_number = parseInt(ccconf.CC_catagory_number) + 1 //increment the number of catgories
+       categoryName = "S" + config.season + "_CC_" + ccconf.CC_catagory_number; //phrase name of catgories
 
-      name = "S" + config.season + "_CC_" + name; //phrase name of catgory
+       if (ccconf.CC_curent_category_id==""){ //makes a category if none exist
+         msg.guild.createChannel(categoryName, "category").then(function(channel) { //make a new category
+           console.log("had to make a new CC category")
+           ccconf.CC_curent_category_id = channel.id //update current category id
+           writecc(); //write new channel id and number to cc.json
+         })
+       }
 
-      message.guild.createChannel(name, "text").then(channel => //creates channels
-        channel.setParent(message.guild.channels.get(configcc.CC_curent_id))).then(channel => //move new channel into currentcatgory
-          channel.send(config.defaultMessage)); //sends defaut message
+       function createChannel(name, ccconf, msg) { //function to make a channel in a category, and make new category if full
+         msg.guild.createChannel(name, "text").then(channel => //make a category
+           channel.setParent(msg.guild.channels.get(ccconf.CC_curent_category_id)).catch(function(error) { //try to move the channel into a category, catching the errors
+             if (error == "DiscordAPIError: Invalid Form Body\nparent_id: Maximum number of channels in category reached (50)") { //check that the error actually is that the categoory is full
+               channel.delete() //delete the category
+               msg.guild.createChannel(categoryName, "category").then(function(channel) { //make a new category
+                 console.log("had to make a new CC category")
+                 ccconf.CC_curent_category_id = channel.id //update current category id
+                 writecc(); //write new channel id and number to cc.json
+                 createChannel(name, ccconf, msg) //try to make the channel again
+               })
+             }
+           })
+         ).then(console.log(msg.author.username + " made a CC called " + name)); //sends defaut message
+       }
 
-      writecc(); //write cc conf to cc.json
-    })
+       createChannel(name, ccconf, msg) //run the category creation function
+     })
 
-  }
-}
+   }
+ }
 
-function writecc(){ //function writes ccconf (odbj) to cc.json
-  fs.writeFile('./cc/cc.json', JSON.stringify(ccconf),{encoding: 'utf-8'}, function(err) {
-    if (err) throw er //throw error
-  })
-}
+ function writecc() { //function writes ccconf (odbj) to cc.json
+   fs.writeFile('./cc/cc.json', JSON.stringify(ccconf), {
+     encoding: 'utf-8'
+   }, function(err) {
+     if (err) throw er //throw error
+   })
+ }
