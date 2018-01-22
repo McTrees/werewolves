168d1ca2e2f7ab2ce251b3cd2e97db6151272d5d
Calvin Edwards <calvin.588.edwards@gmail.com>
2018-01-22 13:09:42 +0000

    so apparently the client needs to be ready before it can get a list of channels, also github is telling me my commit description is too long but that's just making me want to make it really really really long so idk lorem ipsum dolor sit amet words wrods yay

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const client = new discord.Client();
 
 const msg_handler = require("./msg/msg_handler");
 const failsafes = require("./failsafes");
-failsafes(client) // actually run the darn thing
 
 client.on('ready', () => {
   console.log("Logged in!")
+  failsafes(client) // actually actually run the darn thing
 });
 
 client.on('message', msg => {
