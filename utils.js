9cd8844fdbf2d7b9fee630d637c6e0bb55fd79f0
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-01-22 21:56:21 +0000

    Add option in config as to whether or not to be verbose

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const config = require("./config");
+
 exports.toBase64 = function (str) {
   return Buffer.from(str).toString("base64");
 };
@@ -7,17 +9,25 @@ exports.fromBase64 = function (b64) {
 };
 
 exports.infoMessage = function (str) {
-  console.log('\x1b[2m\x1b[36m%s\x1b[0m', "[-] ".concat(str));
+	if (config.developerOptions.logOtherMessages == "true") {
+  		console.log('\x1b[2m\x1b[36m%s\x1b[0m', "[-] ".concat(str));
+  	}
 };
 
 exports.successMessage = function (str) {
-	console.log('\x1b[2m\x1b[32m%s\x1b[0m', "[*] ".concat(str));
+	if (config.developerOptions.logOtherMessages == "true") {
+		console.log('\x1b[2m\x1b[32m%s\x1b[0m', "[*] ".concat(str));
+	}
 }
 
 exports.errorMessage = function (str) {
-	console.log('\x1b[2m\x1b[31m%s\x1b[0m', "[!] ".concat(str));
+	if (config.developerOptions.logOtherMessages == "true") {
+		console.log('\x1b[2m\x1b[31m%s\x1b[0m', "[!] ".concat(str));
+	}
 }
 
 exports.debugMessage = function (str) {
-	console.log('\x1b[2m\x1b[33m%s\x1b[0m', "[#] ".concat(str));
+	if (config.developerOptions.logDebugMessages == "true") {
+		console.log('\x1b[2m\x1b[33m%s\x1b[0m', "[#] ".concat(str));
+	}
 }
