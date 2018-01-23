652ccfc1132c699b1309b668dda53f7450c0a883
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-01-23 17:37:05 +0000

    Add info messages to startup process and check to see if user has set token

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 /* werewolves bot */
+const utils = require('./utils');
 
+utils.infoMessage("Startup process begginning");
 const config = require('./config');
 const token = require('./token').token;
+utils.infoMessage("Config loaded");
 
 const discord = require('discord.js');
 const client = new discord.Client();
 
-const utils = require('./utils');
+
 const msg_handler = require("./msg/msg_handler");
 const failsafes = require("./failsafes");
+utils.infoMessage("Loaded libraries");
+
+if (token == 'insert-token-here') {
+	utils.errorMessage("Incorrect login credentials passed! Please edit token.json with your bot's token.", true)
+	process.exit();
+}
 
 client.on('ready', () => {
   utils.successMessage("Logged in!", true);
-  failsafes(client) // actually actually run the darn thing
+  failsafes(client) // run 'failsafes' module
 });
 
 client.on('message', msg => {
