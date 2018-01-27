6deef8c3f80f47de1e11a055eb3abbf25fc3f40f
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-01-27 11:56:55 +0000

    Add commandline option to skip update checking
    this should make it easy for the bot to still work if for whatever reason we cannot reach mctrees

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /* werewolves bot */
 const utils = require('./utils');
+update = true;
+
 utils.infoMessage("Startup process begginning...");
 // Check to see if the user wants to run in debug mode
 if (process.argv.indexOf("--debug") > -1) {
 	utils.debugMode();
+}
+if (process.argv.indexOf("--noupdate") > -1) {
+	update = false; //dont check for updates
 }
 
 // Check for updates
@@ -45,29 +50,42 @@ client.on('message', msg => {
 require("./user/user").init()
 client.login(token)
 
-function checkForUpdate() {
-const https = require('https');
-url = "https://ben.mctrees.net/api/checkGitVersion.php?repo=werewolves";
 
-	currentVer = require('./package').version;
-      https.get(url, res => {
-	  res.setEncoding("utf8");
-	  let body = "";
-	  res.on("data", data => {
-	    body += data;
-	  });
-	  res.on("end", () => {
-	    	utils.debugMessage(`got ${body} from version API`);
-	    	utils.debugMessage(`Current version from package.json is ${currentVer}`)
-	    	if (currentVer == body) {
-	    		utils.infoMessage("You are up-to-date with the latest version from Github.")
-	    	}
-	    	else {
-	    		utils.warningMessage(`You are not on the latest version. You should update for fixes and features by doing a git pull.
-	    			Remote version: ${body}
-	    			Local version: ${currentVer}
-	    			Please update.`)
-	    	}
-	  });
-	});
+
+
+
+
+// Internals
+
+
+
+function checkForUpdate() {
+if (update) {
+	const https = require('https');
+	url = "https://ben.mctrees.net/api/checkGitVersion.php?repo=werewolves";
+
+		currentVer = require('./package').version;
+	      https.get(url, res => {
+		  res.setEncoding("utf8");
+		  let body = "";
+		  res.on("data", data => {
+		    body += data;
+		  });
+		  res.on("end", () => {
+		    	utils.debugMessage(`got ${body} from version API`);
+		    	utils.debugMessage(`Current version from package.json is ${currentVer}`)
+		    	if (currentVer == body) {
+		    		utils.infoMessage("You are up-to-date with the latest version from Github.")
+		    	}
+		    	else {
+		    		utils.warningMessage(`You are not on the latest version. You should update for fixes and features by doing a git pull.
+		    			Remote version: ${body}
+		    			Local version: ${currentVer}
+		    			Please update.`)
+		    	}
+		  });
+		});
+	}
+} else {
+	utils.infoMessage("Skipping update check");
 }
