8532fb1cdd8b867afccf171a0a963c44cfd72a05
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-01-27 12:03:52 +0000

    Fix update check
    always test your code, kids

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* werewolves bot */
 const utils = require('./utils');
-update = true;
+update = true
 
 utils.infoMessage("Startup process begginning...");
 // Check to see if the user wants to run in debug mode
@@ -12,17 +12,21 @@ if (process.argv.indexOf("--noupdate") > -1) {
 }
 
 // Check for updates
-try {
-checkForUpdate();
-} catch(err) {
-	utils.errorMessage("Could not check for updates. Please check your internet connection and try again.")
+if (update) {
+	try {
+	checkForUpdate();
+	} catch(err) {
+		utils.errorMessage("Could not check for updates. Please check your internet connection and try again.")
+	}
+} else {
+    utils.infoMessage("Skipping update check.")
 }
 
 
 utils.debugMessage("Debug messages enabled.");
 const config = require('./config');
 const token = require('./token').token;
-utils.successMessage("Config loaded");
+utils.successMessage("Config loaded!");
 
 utils.infoMessage("Loading external modules...");
 const discord = require('discord.js');
@@ -31,7 +35,7 @@ const client = new discord.Client();
 utils.infoMessage("Loaded external modules, loading other modules.");
 const msg_handler = require("./msg/msg_handler");
 const failsafes = require("./failsafes");
-utils.infoMessage("Loaded modules");
+utils.infoMessage("Loaded modules.");
 
 if (token == 'insert-token-here') {
 	utils.errorMessage("Incorrect login credentials passed! Please edit token.json with your bot's token.", true)
@@ -60,32 +64,28 @@ client.login(token)
 
 
 function checkForUpdate() {
-if (update) {
-	const https = require('https');
-	url = "https://ben.mctrees.net/api/checkGitVersion.php?repo=werewolves";
+const https = require('https');
+url = "https://ben.mctrees.net/api/checkGitVersion.php?repo=werewolves";
 
-		currentVer = require('./package').version;
-	      https.get(url, res => {
-		  res.setEncoding("utf8");
-		  let body = "";
-		  res.on("data", data => {
-		    body += data;
-		  });
-		  res.on("end", () => {
-		    	utils.debugMessage(`got ${body} from version API`);
-		    	utils.debugMessage(`Current version from package.json is ${currentVer}`)
-		    	if (currentVer == body) {
-		    		utils.infoMessage("You are up-to-date with the latest version from Github.")
-		    	}
-		    	else {
-		    		utils.warningMessage(`You are not on the latest version. You should update for fixes and features by doing a git pull.
-		    			Remote version: ${body}
-		    			Local version: ${currentVer}
-		    			Please update.`)
-		    	}
-		  });
-		});
-	}
-} else {
-	utils.infoMessage("Skipping update check");
+	currentVer = require('./package').version;
+      https.get(url, res => {
+	  res.setEncoding("utf8");
+	  let body = "";
+	  res.on("data", data => {
+	    body += data;
+	  });
+	  res.on("end", () => {
+	    	utils.debugMessage(`got ${body} from version API`);
+	    	utils.debugMessage(`Current version from package.json is ${currentVer}`)
+	    	if (currentVer == body) {
+	    		utils.infoMessage("You are up-to-date with the latest version from Github.")
+	    	}
+	    	else {
+	    		utils.warningMessage(`You are not on the latest version. You should update for fixes and features by doing a git pull.
+	    			Remote version: ${body}
+	    			Local version: ${currentVer}
+	    			Please update.`)
+	    	}
+	  });
+	});
 }
