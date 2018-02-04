2c3c41d15268272ab6cdc0d41d425d145c646cea
Lord-of-the-Galaxy <prathikdiwakar@gmail.com>
2018-02-04 18:36:50 +0530

    Changes

--- msg/msg_handler.js
+++ msg/msg_handler.js
@@ -30,7 +30,7 @@ module.exports = function(msg, client) {
           break;
         case ("p"):
           require("../poll/polls.js")[messageContent[1] + "Cmd"](msg, client, messageContent.slice(2));
-          break;
+		  break;
         case ("c"):
           require("../cc/ccs.js")[messageContent[1] + "Cmd"](msg, client, messageContent.slice(2));
           break;
@@ -48,9 +48,9 @@ module.exports = function(msg, client) {
       if (err instanceof TypeError) {
         msg.reply(`\`${msg.content}\` is an unknown command...`);
       } else {
-        msg.reply(`An error occurred...`);
+		msg.reply(`An error occurred...`);
         if ((config.developerOptions.showErrorsToDevs == "true" && msg.member.roles.has("395967396218667008" ) || config.developerOptions.showErrorsToUsers == "true")){
-          msg.channel.send("the error was: ```" + err + "```")
+          msg.channel.send("The error was: ```" + err + "```")
         }
       }
     }
