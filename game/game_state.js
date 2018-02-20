abfd7d104832c2c4fd6d3293ea46888d8ba53e55
ed588 <calvin.588.edwards@gmail.com>
2018-02-20 18:48:53 +0000

    doh

--- game/game_state.js
+++ game/game_state.js
@@ -126,7 +126,7 @@ exports.set_state_num = function(v) {
 
 exports.set_season_code = function(v) {
   // sets state_num to v
-  if (typeof v == "string" && v.length >= season_code_max_length) {
+  if (typeof v == "string" && v.length <= season_code_max_length) {
     var data = exports.data()
     data.season_code = v
     set_data(data)
