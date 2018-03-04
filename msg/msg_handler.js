fc1739a4d1a6d10fd606453140d50884c814c670
ed588 <calvin.588.edwards@gmail.com>
2018-03-04 17:56:55 +0000

    STOP CHANGING THIS
    I SWEAR THIS HAS TO BE THE FIFTH TIME I HAVE DONE THIS EXACT SAME THING FFS STOP CHANGING IT BACK WHOEVER KEEPS CHANGING IT URRRGH

--- msg/msg_handler.js
+++ msg/msg_handler.js
@@ -87,7 +87,6 @@ module.exports = function(msg, client) {
       if (firstWord == "h") {
         require("../help/help.js")["helpCmd"](msg, client, splitMessage.slice(1), splitMessage.slice(2));
       } else {
-        var root = require(FILENAMES[firstWord])
         if (!FILENAMES[firstWord]) {
           fail(msg, client)
         } else {
