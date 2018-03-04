9fc6ac91111e4e2d0170db06e3e0262e332ba74e
trebor97351 <adds.robbie@gmail.com>
2018-03-04 10:34:48 +0000

    added Assasin role

---
+++ game/roles/inno/assassin.role.js
@@ -0,0 +1,24 @@
+exports.name = "Assasin"
+exports.description = "Can assassinate one player per night, exept the first night"
+
+exports.abilities = {} //Because Javascript
+exports.abilities.powder = {
+  name: "Assasinate",
+  desc: "Assasinate one player. Usable once per **TODO**", //TODO: Make timeframe work
+  run(game, me, args, cb) {
+    game.masters.tell(`Assasinating <@${args[0]}`)
+    game.u.resolve_to_id(args[0]).then(id=>{
+      if (game.state.day_num == 1 && game.state.night_time == false){
+        cb(false)
+        me.tell(`You can't assasinate on the first night`)
+      }else{
+        game.player(id).kill("assasinated")
+        cb(true)
+        me.tell(`successfully assassinated <@${args[0]}>`)
+      }
+    }).catch(e=>{
+      me.tell("couldn't assassinate that person")
+      cb(false)
+    })
+  }
+}
