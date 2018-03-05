8d1d88dff2fee65299f6e37278f418f6cfb2108c
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-03-05 20:10:43 +0000

    Add Bloody Butcher (@ed588 will have to take a look)

---
+++ game/roles/wolf/bloody_butcher.role.js
@@ -0,0 +1,4 @@
+exports.name = "Bloody Butcher"
+exports.description = "The Bloody Butcher is a werewolf who is also part of the Butcher group. He is the traitor among the group, who tries to gain information."
+
+exports.extra_secret_channels = ["inno/butcher"]
