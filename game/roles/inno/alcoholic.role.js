fb88e29e2f3cc7999adf15832c523dd69570044c
Robbie Bradshaw <14bradshawro@christletonhigh.co.uk>
2018-03-05 17:03:06 +0000

    finished alcoholic

--- game/roles/inno/alcoholic.role.js
+++ game/roles/inno/alcoholic.role.js
@@ -1,2 +1,11 @@
+const config = require('../config'); //include main config
+
+
 exports.name = "Alcoholic"
 exports.description = "Can only talk in the tavern."
+
+exports.game_start(game,me){
+  guild.channels.find("id", config.channel_ids.town_square).overwritePermissions(guild.fetchMember(me.user_id), { //author can see it
+    'SEND_MESSAGES': false,
+  }
+}
