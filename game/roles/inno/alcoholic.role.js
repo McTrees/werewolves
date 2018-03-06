e8f10c76ffa99bc58e4b9d9fd6316baf0500f2f3
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-03-06 21:18:02 +0000

    Fix this

--- game/roles/inno/alcoholic.role.js
+++ game/roles/inno/alcoholic.role.js
@@ -4,7 +4,7 @@ const config = require('../config'); //include main config
 exports.name = "Alcoholic"
 exports.description = "Can only talk in the tavern."
 
-exports.game_start(game,me){
+exports.game_start = function(game,me) {
   guild.channels.find("id", config.channel_ids.town_square).overwritePermissions(guild.fetchMember(me.user_id), {
     'SEND_MESSAGES': false,
   }
