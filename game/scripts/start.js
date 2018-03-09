3d321795ba7ef6f77ba3f4788c6ecb76f89c9fa8
ed588 <calvin.588.edwards@gmail.com>
2018-03-09 13:01:30 +0000

    outline

---
+++ game/scripts/start.js
@@ -0,0 +1,10 @@
+const db_fns = require("../db_fns")
+const role_manager = require("../role_manager")
+
+module.exports = function(game, id_list) {
+  // win teams
+  
+  // create secret channels
+  // do game_start for all roles that have one
+  // give participant role <-- LAST
+}
