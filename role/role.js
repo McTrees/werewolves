19ac8bdda0757b9ee3c4db5626709781cef37e3b
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-03-10 18:25:38 +0000

    Add things to role.js

--- role/role.js
+++ role/role.js
@@ -1,14 +1,19 @@
 const role_manager = require("../game/role_manager")
+const config = require("../config")
 
 exports.commands = {}
-exports.commands.roleinfo = function(msg, client, rn) {
+exports.commands.info = function(msg, client, rn) {
   // sends the user info about a role
   // expects a role name, such as inno/basic
   try {
     msg.channel.send(`${role_manager.role(rn).documentation}`)
   } catch (err) {
-    role_manager.all_roles_list().then(function(roles) {
-      msg.reply("that isn't a valid role name. Here's a list of possible roles: \n" + roles.join("\n"))
-  })
+      msg.reply("that isn't a valid role name. use `"+ config.bot_prefix + "r list` for a list of roles")
   }
+}
+
+exports.commands.list = function(msg, client, rest) {
+  role_manager.all_roles_list().then(function(roles) {
+    msg.reply(roles.join("\n"))
+  })
 }
