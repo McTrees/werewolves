239a7c381eceb727cea0a43827c0007fc0923b96
ed588 <calvin.588.edwards@gmail.com>
2018-03-10 15:27:41 +0000

    will do

--- game/roles/inno/alcoholic.role.js
+++ game/roles/inno/alcoholic.role.js
@@ -1,10 +1,10 @@
 const config = require('../config'); //include main config
 
-
 exports.name = "Alcoholic"
 exports.description = "Can only talk in the tavern."
 
 exports.game_start = function(game,me) {
+  var guild = game.guild
   guild.channels.find("id", config.channel_ids.town_square).overwritePermissions(guild.fetchMember(me.user_id), {
     'SEND_MESSAGES': false,
   }
