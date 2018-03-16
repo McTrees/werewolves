86375ba6a7c39f10df2c28b949d1298d8f961bd5
ed588 <calvin.588.edwards@gmail.com>
2018-03-16 11:16:20 +0000

    secret channels

--- game/scripts/start.js
+++ game/scripts/start.js
@@ -3,6 +3,9 @@ const role_manager = require("../role_manager")
 const utils = require("../../utils")
 const PlayerController = require("../player_controller").PlayerController
 const config = require("../../config")
+const channels = require("../../channel/channel_handler")
+const secret = require("../secret_channel_conf")
+const game_state = require("../game_state")
 
 module.exports = async function(game, id_list) {
   // win teams
@@ -23,7 +26,27 @@ module.exports = async function(game, id_list) {
       }
     }
   })
+
   // create secret channels
+  // we will assume that all role names in the conf file are valid,
+  // because it's your fault if you change it and also
+  // i cba validating it here
+
+  // individual channels are 1 person per channel
+  secret.individual.forEach(async role_name=>{
+    var all = await game.u.all_with_role(role_name)
+    var role_info = role_manager.role(role_name)
+    all.forEach((id, index)=>{
+      channels.createChannel(
+        client,
+        client.guilds.get(config.guild_id),
+        [id],
+        `${game_state.data().season_code}_${role_info.name}`,
+        config.category_ids.secret_channel,
+        role_info.documentation
+      )
+    })
+  })
 
   // do game_start for all roles that have one
   id_list.forEach(async function(id) {
