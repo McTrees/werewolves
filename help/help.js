08749580c60f733be1dd0f095a899fa4997142f4
BenTechy66 <sendbenspam@yahoo.co.uk>
2018-03-25 16:07:18 +0100

    In honour of the 1000th commit
    Lol read it

--- help/help.js
+++ help/help.js
@@ -118,7 +118,14 @@ Possible categories: ` + dirsF.join(", "))
     } else {
       utils.debugMessage(`Help cmd called w/ ${cmd} & ${args}`)
     }
-
+    /**
+    * For the brave souls who get this far: You are the chosen ones,
+    * the valiant knights of programming who toil away, without rest,
+    * fixing our most awful code. To you, true saviors, kings of men,
+    * I say this: never gonna give you up, never gonna let you down,
+    * never gonna run around and desert you. Never gonna make you cry,
+    * never gonna say goodbye. Never gonna tell a lie and hurt you.
+    */
     utils.debugMessage('Reading file: ' + './help/cmds/' + args[0] + "/" +cmd[0] + '.md')
     try {
     fs.readFile('./help/cmds/' + args[0] + '/' + cmd[0] + '.md', {
