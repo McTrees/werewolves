d0d3a58ec8f4e5033a75deee6d2eb30a09a4392d
ed588 <calvin.588.edwards@gmail.com>
2018-03-30 21:00:09 +0100

    every day also

--- game/scripts/every_day.js
+++ game/scripts/every_day.js
@@ -1,3 +1,11 @@
+const role_manager = require("../role_manager")
 module.exports = function(game, id_list) {
-  
+  id_list.forEach(async id=>{
+    var player = game.player(id)
+    var role = role_manager.role(await player.role)
+    if (typeof role.on_night_to_day == "function") {
+      role.on_night_to_day(game, player)
+      // this gets run before kill q is executed btw
+    }
+  })
 }
