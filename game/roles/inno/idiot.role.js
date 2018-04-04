e1e461ed702b0564918c53af2bdab94af79f0825
Lord-of-the-Galaxy <prathikdiwakar@gmail.com>
2018-04-04 14:08:57 +0530

    In progress

--- game/roles/inno/idiot.role.js
+++ game/roles/inno/idiot.role.js
@@ -1,2 +1,7 @@
 exports.name = "Idiot"
-exports.description = "TODO - this whole role"
+exports.description = "If the idiot dies during the death poll" +
+	" or during the night, the ﬁrst attack will be prevented." +
+	"\n However, the idiot loses the right to vote";
+	//Copied this from instruction manual, you might wanna change this @Randium
+	
+const no_vote = "cannot-vote-idiot";//Because there's other reasons for which a player can't vote
