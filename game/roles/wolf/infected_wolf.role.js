7a59ad07bcae2233a5eb3e7335f88a200ca9589b
ed588 <calvin.588.edwards@gmail.com>
2018-04-12 19:51:45 +0100

    change some messages

--- game/roles/wolf/infected_wolf.role.js
+++ game/roles/wolf/infected_wolf.role.js
@@ -4,7 +4,7 @@ exports.description = "Can turn one werewolf into a player each night."
 exports.abilities = {} //Because Javascript
 exports.abilities.curse = {
   name: "infect",
-  desc: "Turn a player into a werwolf (Usage: `infect @playerMention`)",
+  desc: "Turn a player into a werewolf",
   timings: {
     periods: 2,
     allow_day: false
@@ -14,11 +14,12 @@ exports.abilities.curse = {
     game.u.resolve_to_id(args[0]).then(t => {
       p = game.player(t)
       if (p.role == "wolf/werewolf") {
-        cb(true, "Your infection result is **neutral**. You haven't infected <@${t}>, because they already were a **WEREWOLF**!")
+        cb(true, `Your infection result is **neutral**. You haven't infected ${t}, because they already were a **WEREWOLF**!`)
+        return
       }
       p.role = "wolf/werewolf"
       p.tell("**You have been turned into a werewolf.**")
-      cb(true, "Your infection result is **positive**. You have infected <@${t}> successfully, and they are now a **Werewolf**!")
+      cb(true, `Your infection result is **positive**. You have infected ${t} successfully, and they are now a **Werewolf**!`)
     })
   }
 }
