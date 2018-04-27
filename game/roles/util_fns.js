b8cd8c38c859bac90ec46a2f3894189b2464ff9e
ed588 <calvin.588.edwards@gmail.com>
2018-04-27 08:37:30 +0100

    add utils functions add a disguised_role function

---
+++ game/roles/util_fns.js
@@ -0,0 +1,21 @@
+// utility functions that many roles might need
+// to avoid duplication
+// DRY
+const db_fns = require("../db_fns")
+const user = require("../../user/user")
+
+exports.disguised_role = function(player_id) {
+  return new Promise(function(resolve, reject) {
+    db_fns.relationships.all_affectees_of(player_id, "disguised_as").then(list=>{
+      if (list.length > 0) {
+        resolve(list[0])
+      } else {
+        user.get_role(player_id).then(resolve)
+      }
+    })
+  })
+  // if a player is disguised, return their disguised role
+  // otherwise, return their actual role
+  // as the role id string
+
+}
