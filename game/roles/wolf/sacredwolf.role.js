5770161f1930f2f75e0d5204454785baf058b848
ed588 <calvin.588.edwards@gmail.com>
2018-05-09 17:45:08 +0100

    sacred wolf

--- game/roles/wolf/sacredwolf.role.js
+++ game/roles/wolf/sacredwolf.role.js
@@ -2,3 +2,12 @@
 
 exports.name = "Sacred Werewolf"
 exports.description = "Sacred Werewolf. Cannot be killed at night."
+
+exports.on_death = function(kill_desc, game, me) {
+  if (kill_desc.by !== "l") { // not lynch - survives
+    game.masters.tell(me.id, "someone attempted to kill me, but i'm sacred so I'm ok :)")
+    return false // didn't die
+  } else {
+    return true // did die
+  }
+}
