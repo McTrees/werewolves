448f3365e3ea7ed6688cd8f1bf30adf82beb24bf
ed588 <calvin.588.edwards@gmail.com>
2018-05-09 17:52:36 +0100

    white wolf works now

--- game/roles/wolf/whitewerewolf.role.js
+++ game/roles/wolf/whitewerewolf.role.js
@@ -3,6 +3,8 @@
 exports.name = "White Werewolf"
 exports.description = "Kills a single wolf per 2 nights"
 
+const wolfpack_roles = require("../util_fns").wolfpack_roles
+
 exports.abilities = {}
 exports.abilities.kill = {
   name: "kill",
@@ -15,7 +17,14 @@ exports.abilities.kill = {
     game.masters.tell(me.id, `attempting to white wolf kill ${args[0]}`)
     game.u.resolve_to_id(args[0]).then(t=> {
       p = game.player(t)
-      game.add_to_kill_q(args[0], "whitew", client)
+      p.role.then(r=>{
+        if (wolfpack_roles.includes(r)) {
+          game.kill(t)
+          cb(true, `successfully killed ${p}`)
+        } else {
+          cb(fals, `${p} is not in the wolf pack, so you can't kill them`)
+        }
+      })
     })
   }
 }
